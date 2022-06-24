530b694e86b6991a99f02d37a2caa51d3976442d
Scott Hallock <scott@hallocks.net>
2022-06-24 23:07:42 +0000

    A minimal skeleton for exercises in C++

---
+++ exercise.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "exercise.h"
+
+namespace exercise {
+int exercise() {
+  std::cout << "Exercise!" << std::endl;
+  return 0;
+}
+}  // namespace exercise
---
+++ main.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "exercise.h"
+
+int main(int argc, char **argv) { return exercise::exercise(); }
